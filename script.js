8dbbd418fc0c7789353f477245269de093051397
Paola Beltran <pavis.asajfb@gmail.com>
2023-06-06 01:19:07 -0500

    ejercicios terminados

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* // EJE 1
+// EJE 1
 let respuestaEjercicio1 = prompt("Eres bellisimo");
 
 if(respuestaEjercicio1.toLowerCase() === "si") {
@@ -113,35 +113,123 @@ if (calificacion >= 1 && calificacion <= 10) {
 } else {
   alert("ERROR");
 }
-*/
+
 
 //EJE 9
-/*
-const topping = prompt("Selecciona un topping para tu helado (oreo / KitKat / brownie):");
-const helado = 50;
+let topping = prompt("Selecciona un topping para tu helado (oreo / KitKat / brownie):");
+let helado = 50;
 
 if (topping.toLowerCase() === "oreo") {
-  oreo = helado + 10;
-  alert("Helado con topping de oreo cuesta " + oreo + " MXN");
-} if (topping.toLowerCase() === "KitKat") {
-  kitkat = helado + 15;
-  alert("Helado con topping de kitkat cuesta " + kitkat + "MXN");
-} if (topping.toLowerCase() === "brownie") {
+  helado += 10;
+  alert(`Helado con topping de oreo cuesta ${helado} MXN`);
+} else if (topping.toLowerCase() === "kitkat") {
+  helado += 15;
+  alert("Helado con topping de kitkat cuesta " + helado + "MXN");
+} else if (topping.toLowerCase() === "brownie") {
   helado = helado + 20;
   alert("Helado con topping de brownie cuesta " + helado + "MXN");
-  } if (topping === "") {
+} else if (topping === "") {
   alert("El precio del helado sin Topping es de 50 MXN.");
-  }  else {
-    alert(" “no tenemos este topping, lo sentimos.")
-  }
-*/
-let helado = 50;
-let topping = prompt("ingrese topping");
-
-
-if(topping === 'oreo') {
-  topp = 50+10;
-  alert("cuesta" ${top});
-}else{
-  alert("cuesta ghghgh");
+} else {
+  alert("no tenemos este topping, lo sentimos.");
 }
+*/
+
+//EJE 10
+Beca Facebook: 20% de descuento.
+Beca Google: 15% de descuento.
+Beca Jesua: 50% de descuento.
+
+let programa = prompt("Ingrese programa educativo (course / carrera / master):");
+let beca = prompt("Ingrese tipo de Beca: Facebook / Google / Jesua / No");
+
+let course = 4999;
+let carrera = 3999;
+let master = 2999;
+let totalCarrera = 0;
+let total = 0;
+
+Course: 2 meses
+Carrera 6 meses
+Master: 12 meses
+
+
+if(programa.toLowerCase() === "course" && beca.toLowerCase() === "facebook") {
+  totalCarrera = Math.round(course - (course * 20 / 100)) * 2;
+  alert(`El programa Educativo nivel Course con Beca Facebook cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "course" && beca.toLowerCase() === "google") {
+  totalCarrera = Math.round(((course - (course * 15 / 100)) * 2));
+  alert(`El programa Educativo nivel Course con Beca Google cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "course" && beca.toLowerCase() === "jesua") {
+  totalCarrera = Math.round(((course - (course * 50 / 100)) * 2));
+  alert(`El programa Educativo nivel Course con Beca Jesua cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "carrera" && beca.toLowerCase() === "facebook") {
+  totalCarrera = Math.round(((carrera - (carrera * 20 / 100)) * 6));
+  alert(`El programa Educativo nivel Carrera con Beca Facebook cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "carrera" && beca.toLowerCase() === "google") {
+  totalCarrera = Math.round(((carrera - (carrera * 15 / 100)) * 6));
+  alert(`El programa Educativo nivel Carrera con Beca Google cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "carrera" && beca.toLowerCase() === "jesua") {
+  totalCarrera = Math.round(((carrera - (carrera * 50 / 100)) * 6));
+  alert(`El programa Educativo nivel Carrera con Beca Jesua cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "master" && beca.toLowerCase() === "facebook") {
+  totalCarrera = Math.round(((master - (master * 20 / 100)) * 12));
+  alert(`El programa Educativo nivel Master con Beca Facebook cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "master" && beca.toLowerCase() === "google") {
+  totalCarrera = Math.round(((master - (master * 15 / 100)) * 12));
+  alert(`El programa Educativo nivel Master con Beca Google cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "master" && beca.toLowerCase() === "jesua") {
+  totalCarrera = Math.round(((master - (master * 50 / 100)) * 12));
+  alert(`El programa Educativo nivel Master con Beca Jesua cuesta: ${totalCarrera}`);
+} else if(programa.toLowerCase() === "course" && beca.toLowerCase() === "no") {
+  total = course * 2; 
+  alert(`El programa Educativo nivel Course cuesta: ${total}`);
+} else if(programa.toLowerCase() === "carrera" && beca.toLowerCase() === "no") {
+  total = carrera * 6; 
+  alert(`El programa Educativo nivel Carrera cuesta: ${total}`);
+} else if(programa.toLowerCase() === "master" && beca.toLowerCase() === "no") {
+  total = master * 12; 
+  alert(`El programa Educativo nivel Master cuesta: ${total}`);
+} else {
+  alert("no contamos con ese Programa")
+} 
+
+
+//EJE 11
+let distancia = prompt("Ingrese distancia recorrida");
+let litros50 = prompt("Ingrese Litros consumidos"); 
+let vehiculo = prompt("Ingrese tipo de Vehículo coche / moto / autobus");
+
+let coche = 0.20;
+let moto = 0.10;
+let autobus = 0.5;
+
+if(litros > 0 && litros <= 100 && vehiculo.toLowerCase() === "coche"){
+  litros = 5;
+  totalPagar = (coche * distancia) + litros;
+  alert(`Total a pagar : ${totalPagar}`);
+} else if(litros > 100 && vehiculo.toLowerCase() === "coche"){
+  litros = 10;
+  totalPagar = (coche * distancia) + litros;
+  alert(`Total a pagar  ${totalPagar}`);
+} else if(litros > 0 && litros <= 100 && vehiculo.toLowerCase() === "moto"){
+  litros = 5;
+  totalPagar = (moto * distancia) + litros;
+  alert(`Total a pagar : ${totalPagar}`);
+} else if(litros > 100 && vehiculo.toLowerCase() === "moto"){
+  litros = 10;
+  totalPagar = (moto * distancia) + litros;
+  alert(`Total a pagar : ${totalPagar}`);
+} else if(litros > 0 && litros <= 100 && vehiculo.toLowerCase() === "autobus"){
+  litros = 5;
+  totalPagar = (autobus * distancia) + litros;
+  alert(`Total a pagar : ${totalPagar}`)
+} else if(litros > 100 && vehiculo.toLowerCase() === "autobus"){
+  litros = 10;
+  totalPagar = (autobus * distancia) + litros;
+  alert(`Total a pagar : ${totalPagar}`);
+} else{
+  alert(`error`)
+}
+
+//total a pagar = (precio kilometro x kms recorridos) + extra por litros consumidos.
